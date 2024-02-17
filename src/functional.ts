74a417911f6e5205f932ae33b14c7a86ae87051e
Caden Marinozzi <nekumelon@gmail.com>
2024-02-17 08:00:47 -0800

    Functional fix

--- src/functional.ts
+++ src/functional.ts
@@ -20,66 +20,64 @@ const detectFramework = (config: any): string => {
 	return 'webpack';
 };
 
-module.exports.useScraipt(
-	(
-		config: any = {},
-		options: any = {},
-		framework: string | undefined
-	): any => {
-		if (!framework) {
-			framework = detectFramework(config);
-		}
-
-		if (framework === 'React') {
-			if (!config.webpack) {
-				config.webpack = {};
-			}
-
-			if (!config.webpack.configure) {
-				config.webpack.configure = {};
-			}
-
-			if (!config.webpack.configure.module) {
-				config.webpack.configure.module = {};
-			}
-
-			if (!config.webpack.configure.module.rules) {
-				config.webpack.configure.module.rules = [];
-			}
-
-			config.webpack.configure.module.rules.push({
-				test: /.[jt]sx*/,
-				use: [
-					{
-						loader: 'scraipt',
-						options: {
-							// Default to include src folder only
-							include: ['src'],
-							...options,
-						},
-					},
-				],
-			});
-		} else if (framework === 'webpack') {
-			if (!config.module) {
-				config.module = {};
-			}
-
-			if (!config.module.rules) {
-				config.module.rules = [];
-			}
-
-			config.module.rules.push({
-				test: /.[jt]sx*/,
-				use: [
-					{
-						loader: 'scraipt',
-						options,
-					},
-				],
-			});
-		}
-
-		return config;
+module.exports.useScraipt = (
+	config: any = {},
+	options: any = {},
+	framework: string | undefined
+): any => {
+	if (!framework) {
+		framework = detectFramework(config);
 	}
-);
+
+	if (framework === 'React') {
+		if (!config.webpack) {
+			config.webpack = {};
+		}
+
+		if (!config.webpack.configure) {
+			config.webpack.configure = {};
+		}
+
+		if (!config.webpack.configure.module) {
+			config.webpack.configure.module = {};
+		}
+
+		if (!config.webpack.configure.module.rules) {
+			config.webpack.configure.module.rules = [];
+		}
+
+		config.webpack.configure.module.rules.push({
+			test: /.[jt]sx*/,
+			use: [
+				{
+					loader: 'scraipt',
+					options: {
+						// Default to include src folder only
+						include: ['src'],
+						...options,
+					},
+				},
+			],
+		});
+	} else if (framework === 'webpack') {
+		if (!config.module) {
+			config.module = {};
+		}
+
+		if (!config.module.rules) {
+			config.module.rules = [];
+		}
+
+		config.module.rules.push({
+			test: /.[jt]sx*/,
+			use: [
+				{
+					loader: 'scraipt',
+					options,
+				},
+			],
+		});
+	}
+
+	return config;
+};
